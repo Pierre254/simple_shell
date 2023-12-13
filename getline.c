57cd3455aadc813c3b814d9b790e71ae6304354d
Kelvin <qelvynn@gmail.com>
2023-12-13 03:26:06 -0800

    simple_shell

--- getline.c
+++ getline.c
@@ -15,30 +15,30 @@ ssize_t buffer_input(info_t *info, char **input, size_t *size)
 
     if (!*size) /* if nothing left in the input, fill it */
     {
-   	 free(*input);
-   	 *input = NULL;
-   	 signal(SIGINT, sigint_handler);
+        free(*input);
+        *input = NULL;
+        signal(SIGINT, sigint_handler);
 #if USE_GETLINE
-   	 r = getline(input, &size_p, stdin);
+        r = getline(input, &size_p, stdin);
 #else
-   	 r = _getline(info, input, &size_p);
+        r = _getline(info, input, &size_p);
 #endif
-   	 if (r > 0)
-   	 {
-   		 if ((*input)[r - 1] == '\n')
-   		 {
-   			 (*input)[r - 1] = '\0'; /* remove trailing newline */
-   			 r--;
-   		 }
-   		 info->linecount_flag = 1;
-   		 remove_comments(*input);
-   		 build_history_list(info, *input, info->histcount++);
-   		 /* if (_strchr(*input, ';')) is this a command chain? */
-   		 {
-   			 *size = r;
-   			 info->cmd_input = input;
-   		 }
-   	 }
+        if (r > 0)
+        {
+            if ((*input)[r - 1] == '\n')
+            {
+                (*input)[r - 1] = '\0'; /* remove trailing newline */
+                r--;
+            }
+            info->linecount_flag = 1;
+            remove_comments(*input);
+            build_history_list(info, *input, info->histcount++);
+            /* if (_strchr(*input, ';')) is this a command chain? */
+            {
+                *size = r;
+                info->cmd_input = input;
+            }
+        }
     }
     return (r);
 }
@@ -59,29 +59,29 @@ ssize_t get_input(info_t *info)
     _putchar(BUF_FLUSH);
     r = buffer_input(info, &input, &size);
     if (r == -1) /* EOF */
-   	 return (-1);
+        return (-1);
     if (size)    /* we have commands left in the chain input */
     {
-   	 j = i; /* init new iterator to current input position */
-   	 p = input + i; /* get pointer for return */
+        j = i; /* init new iterator to current input position */
+        p = input + i; /* get pointer for return */
 
-   	 check_chain(info, input, &j, i, size);
-   	 while (j < size) /* iterate to semicolon or end */
-   	 {
-   		 if (is_chain(info, input, &j))
-   			 break;
-   		 j++;
-   	 }
+        check_chain(info, input, &j, i, size);
+        while (j < size) /* iterate to semicolon or end */
+        {
+            if (is_chain(info, input, &j))
+                break;
+            j++;
+        }
 
-   	 i = j + 1; /* increment past nulled ';'' */
-   	 if (i >= size) /* reached end of input? */
-   	 {
-   		 i = size = 0; /* reset position and size */
-   		 info->cmd_input_type = CMD_NORM;
-   	 }
+        i = j + 1; /* increment past nulled ';'' */
+        if (i >= size) /* reached end of input? */
+        {
+            i = size = 0; /* reset position and size */
+            info->cmd_input_type = CMD_NORM;
+        }
 
-   	 *input_p = p; /* pass back pointer to current command position */
-   	 return (_strlen(p)); /* return length of current command */
+        *input_p = p; /* pass back pointer to current command position */
+        return (_strlen(p)); /* return length of current command */
     }
 
     *input_p = input; /* else not a chain, pass back input from _getline() */
@@ -101,10 +101,10 @@ ssize_t read_input(info_t *info, char *input, size_t *i)
     ssize_t r = 0;
 
     if (*i)
-   	 return (0);
+        return (0);
     r = read(info->readfd, input, READ_BUF_SIZE);
     if (r >= 0)
-   	 *i = r;
+        *i = r;
     return (r);
 }
 
@@ -126,31 +126,31 @@ int _getline(info_t *info, char **ptr, size_t *length)
 
     p = *ptr;
     if (p && length)
-   	 s = *length;
+        s = *length;
     if (i == size)
-   	 i = size = 0;
+        i = size = 0;
 
     r = read_input(info, input, &size);
     if (r == -1 || (r == 0 && size == 0))
-   	 return (-1);
+        return (-1);
 
     c = _strchr(input + i, '\n');
     k = c ? 1 + (unsigned int)(c - input) : size;
     new_p = _realloc(p, s, s ? s + k : k + 1);
     if (!new_p) /* MALLOC FAILURE! */
-   	 return (p ? free(p), -1 : -1);
+        return (p ? free(p), -1 : -1);
 
     if (s)
-   	 _strncat(new_p, input + i, k - i);
+        _strncat(new_p, input + i, k - i);
     else
-   	 _strncpy(new_p, input + i, k - i + 1);
+        _strncpy(new_p, input + i, k - i + 1);
 
     s += k - i;
     i = k;
     p = new_p;
 
     if (length)
-   	 *length = s;
+        *length = s;
     *ptr = p;
     return (s);
 }
