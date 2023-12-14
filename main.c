9ce1c514a924a6a9b2bb5fa340cbadd6511385c5
Pierre254 <ptruter@gmail.com>
2023-12-14 04:09:27 -0800

    shell_t

--- main.c
+++ main.c
@@ -1,11 +1,5 @@
 #include "shell.h"
 
-// Enable some compiler optimizations
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC optimize("inline")
-#pragma GCC optimize("tree-vectorize")
-
 /**
  * main - entry point
  * @ac: arg count
@@ -13,15 +7,15 @@
  *
  * Return: 0 on success, 1 on error
  */
-int main(register int ac, register char **av)
+int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
-	register int fd = 2;
+	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
-	     "add $3, %0"
-	     : "=r" (fd)
-	     : "r" (fd));
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
 	if (ac == 2)
 	{
@@ -32,8 +26,10 @@ int main(register int ac, register char **av)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				// Use formatted output instead of string concatenation
-				fprintf(stderr, "%s: 0: Can't open %s\n", av[0], av[1]);
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
 			}
